1bcb12b02964c1c89b314bd11f5fe876c6dcc448
Mariya Davydova <mariyafomkina@gmail.com>
2017-10-12 18:31:49 +0300

    Edit and refresh enabled

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-Dashboard.registerWidget(function (dashboardAPI) {
-  dashboardAPI.setTitle('Keep calm and purrrrr...');
+function renderCat(catGifId) {
   var container = document.getElementById('cat');
   container.innerHTML =
     "<img " +
-      "src='http://iconka.com/wp-content/uploads/edd/2015/10/purr.gif' " +
-      "style='width:90%'>";
+    "src='http://iconka.com/wp-content/uploads/edd/2015/10/" + catGifId +".gif' " +
+    "style='width:90%'>";
+}
+
+Dashboard.registerWidget(function (dashboardAPI, registerWidgetAPI) {
+  dashboardAPI.setTitle('Keep calm and purrrrr...');
+  renderCat("purr");
+
+  registerWidgetAPI({
+    onConfigure: function() {
+      renderCat("meal");
+    },
+    onRefresh: function() {
+      renderCat("knead");
+    }
+  });
 });
