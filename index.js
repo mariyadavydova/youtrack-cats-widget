0458c83d520df51d746815263d3a55a4556557be
Mariya Davydova <mariyafomkina@gmail.com>
2017-10-12 17:34:33 +0300

    Single cat.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 Dashboard.registerWidget(function (dashboardAPI) {
-  dashboardAPI.setTitle('Purrr...');
+  dashboardAPI.setTitle('Keep calm and purrrrr...');
+  var container = document.getElementById('cat');
+  container.innerHTML =
+    "<img " +
+      "src='http://iconka.com/wp-content/uploads/edd/2015/10/purr.gif' " +
+      "style='width:90%'>";
 });
