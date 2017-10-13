d843ffe50946a58059592bf35c5ce564353eafed
Mariya Davydova <mariyafomkina@gmail.com>
2017-10-13 10:54:03 +0300

    Correct editing and loading cats

--- index.js
+++ index.js
@@ -11,26 +11,30 @@ function renderCat(catGifId) {
 function renderSelector(dashboardAPI) {
   var container = document.getElementById('cat');
   container.innerHTML =
-    "<select>" +
+    "<select id='catSelector' class='catSelector'>" +
       "<option value='random'>Random Cat</option>" +
       "<option value='purr'>Purr</option>" +
       "<option value='meal'>Meal</option>" +
       "<option value='knead'>Knead</option>" +
     "</select>" +
-    "<input type='button' value='Save' id='save'>";
+    "<input type='button' value='Save' id='save' class='save'>";
 
   var button = document.getElementById('save');
   button.onclick = function() {
+    var selector = document.getElementById('catSelector');
+    var catId = selector.options[selector.selectedIndex].value;
+    console.log('Cat ID:' + catId);
     dashboardAPI.storeConfig({
-      cat: 'random'
+      catId: catId
     });
     dashboardAPI.exitConfigMode();
+    renderCat(catId);
   }
 }
 
 function drawCatFromConfig(dashboardAPI) {
   dashboardAPI.readConfig().then(function(config) {
-    var catId = config.cat || 'purr';
+    var catId = (config && config.catId) || 'purr';
     if (catId === 'random') {
       catId = listOfCatIds[Math.floor(Math.random() * listOfCatIds.length)];
     }
