73afc80abeae6b3a349e3394077185ed167e3bd6
Mariya Davydova <mariyafomkina@gmail.com>
2017-10-13 11:08:08 +0300

    Proper saving mode + cancel button

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var listOfCatIds = ["purr", "meal", "knead"];
 
-function renderCat(catGifId) {
+function renderCat(catId) {
+  if (catId === 'random') {
+    catId = listOfCatIds[Math.floor(Math.random() * listOfCatIds.length)];
+  }
   var container = document.getElementById('cat');
   container.innerHTML =
     "<img " +
-    "src='http://iconka.com/wp-content/uploads/edd/2015/10/" + catGifId +".gif' " +
+    "src='http://iconka.com/wp-content/uploads/edd/2015/10/" + catId +".gif' " +
     "style='width:90%'>";
 }
 
-function renderSelector(dashboardAPI) {
+function renderSelector(dashboardAPI, catId) {
   var container = document.getElementById('cat');
   container.innerHTML =
     "<select id='catSelector' class='catSelector'>" +
@@ -17,27 +20,36 @@ function renderSelector(dashboardAPI) {
       "<option value='meal'>Meal</option>" +
       "<option value='knead'>Knead</option>" +
     "</select>" +
-    "<input type='button' value='Save' id='save' class='save'>";
+    "<input type='button' value='Save' id='save' class='button'>" +
+    "<input type='button' value='Cancel' id='cancel' class='button'>";
 
-  var button = document.getElementById('save');
-  button.onclick = function() {
+  var selector = document.getElementById('catSelector');
+  selector.value = catId;
+
+  var buttonSave = document.getElementById('save');
+  buttonSave.onclick = function() {
     var selector = document.getElementById('catSelector');
     var catId = selector.options[selector.selectedIndex].value;
-    console.log('Cat ID:' + catId);
     dashboardAPI.storeConfig({
       catId: catId
     });
     dashboardAPI.exitConfigMode();
     renderCat(catId);
-  }
+  };
+
+  var buttonCancel = document.getElementById('cancel');
+  buttonCancel.onclick = function() {
+    dashboardAPI.exitConfigMode();
+    var selector = document.getElementById('catSelector');
+    var catId = selector.options[selector.selectedIndex].value;
+    dashboardAPI.exitConfigMode();
+    renderCat(catId);
+  };
 }
 
 function drawCatFromConfig(dashboardAPI) {
   dashboardAPI.readConfig().then(function(config) {
     var catId = (config && config.catId) || 'purr';
-    if (catId === 'random') {
-      catId = listOfCatIds[Math.floor(Math.random() * listOfCatIds.length)];
-    }
     renderCat(catId);
   });
 }
@@ -48,7 +60,10 @@ Dashboard.registerWidget(function (dashboardAPI, registerWidgetAPI) {
 
   registerWidgetAPI({
     onConfigure: function() {
-      renderSelector(dashboardAPI);
+      dashboardAPI.readConfig().then(function(config) {
+        var catId = (config && config.catId) || 'purr';
+        renderSelector(dashboardAPI, catId);
+      });
     },
     onRefresh: function() {
       drawCatFromConfig(dashboardAPI);
