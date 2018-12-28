ead4a98d467fab048ec4d8f94a0c8ccd2b25dac2
Mariya Davydova <mariyafomkina@gmail.com>
2018-12-28 18:33:55 +0300

    Correct support of random cat

--- src/app/app.js
+++ src/app/app.js
@@ -2,7 +2,9 @@ import DashboardAddons from 'hub-dashboard-addons';
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {render} from 'react-dom';
+import Text from '@jetbrains/ring-ui/components/text/text';
 import Select from '@jetbrains/ring-ui/components/select/select';
+import List from '@jetbrains/ring-ui/components/list/list';
 import Panel from '@jetbrains/ring-ui/components/panel/panel';
 import Button from '@jetbrains/ring-ui/components/button/button';
 import EmptyWidget, {EmptyWidgetFaces} from '@jetbrains/hub-widget-ui/dist/empty-widget';
@@ -11,21 +13,43 @@ import 'file-loader?name=[name].[ext]!../../manifest.json';
 // eslint-disable-line import/no-unresolved
 import styles from './app.css';
 
-var LIST_OF_CAT_IDS = ["purr", "walk", "acrobat", "kittens",
-                       "burp", "sleepy", "facepalm", "meal",
-                       "banjo", "groom", "gift", "knead",
-                       "love", "fly", "popcorn", "drink"];
-
-var CAT_IDS = [
-  {'label':'Purr', 'key':'purr'},
-  {'label':'Walk', 'key':'walk'},
-
+var PET_IDS = [
+  {'label':'Random cat', 'key':'random-cat'},
+  {'label':'Cats',     'key':'cats',   'rgItemType':List.ListProps.Type.TITLE},
+  {'label':'Purr',     'key':'purr'},
+  {'label':'Walk',     'key':'walk'},
+  {'label':'Acrobat',  'key':'acrobat'},
+  {'label':'Kittens',  'key':'kittens'},
+  {'label':'Burp',     'key':'burp'},
+  {'label':'Sleepy',   'key':'sleepy'},
+  {'label':'Facepalm', 'key':'facepalm'},
+  {'label':'Meal',     'key':'meal'},
+  {'label':'Banjo',    'key':'banjo'},
+  {'label':'Groom',    'key':'groom'},
+  {'label':'Gift',     'key':'gift'},
+  {'label':'Knead',    'key':'knead'},
+  {'label':'Love',     'key':'love'},
+  {'label':'Fly',      'key':'fly'},
+  {'label':'Popcorn',  'key':'popcorn'},
+  {'label':'Drink',    'key':'drink'}
 ];
 
-var DEFAULT_CAT_ID = "purr";
-var DEFAULT_TITLE = "Cats";
+var DEFAULT_PET_ID = 'purr';
+var DEFAULT_RANDOM = 'none'
+var DEFAULT_TITLE  = 'Pets';
 
-//---------- WIDGET ----------
+/****************************************
+config: {
+  petId: <one of keys from PET_IDS>,
+  title: "Text"
+}
+state: {
+  isConfiguring: true/false,
+  petId: <one of image keys from PET_IDS>,
+  random: 'random-cat'/'none',
+  title: "Text"
+}
+****************************************/
 
 class Widget extends Component {
   static propTypes = {
@@ -38,11 +62,15 @@ class Widget extends Component {
     const {registerWidgetApi, dashboardApi} = props;
 
     this.state = {
-      isConfiguring: false
+      isConfiguring: false,
+      petId:  DEFAULT_PET_ID,
+      random: DEFAULT_RANDOM,
+      title:  DEFAULT_TITLE
     };
 
     registerWidgetApi({
-      onConfigure: () => this.setState({isConfiguring: true})
+      onConfigure: () => this.setState({isConfiguring: true}),
+      onRefresh: () => this.updateRandomPet()
     });
 
     this.initialize(dashboardApi);
@@ -51,19 +79,33 @@ class Widget extends Component {
   initialize(dashboardApi) {
     dashboardApi.readConfig().then(config => {
       if (!config) {
-        this.setState({catId: DEFAULT_CAT_ID,
-                       title: DEFAULT_TITLE})
         return;
       }
-      this.setState({catId: config.catId || DEFAULT_CAT_ID,
-                     title: config.title || DEFAULT_TITLE});
+      this.loadConfig(config);
     });
   }
 
+  loadConfig = config => {
+    var random = 'none';
+    var petId = config.petId || config.catId || DEFAULT_PET_ID;
+    if (petId === 'random' || petId === 'random-cat') {
+      random = 'random-cat';
+      // cats are from 2 to 17
+      petId = PET_IDS[Math.floor(Math.random() * 16) + 2].key;
+    }
+    var title = config.title || DEFAULT_TITLE;
+    this.setState({petId, random, title});
+  }
+
   saveConfig = async () => {
-    const {catId, title} = this.state;
-    await this.props.dashboardApi.storeConfig({catId, title});
+    const {petId, random, title} = this.state;
+    var configPetId = petId;
+    if (random === 'random-cat') {
+      configPetId = random;
+    }
+    await this.props.dashboardApi.storeConfig({petId: configPetId, title});
     this.setState({isConfiguring: false});
+    this.updateRandomPet();
   };
 
   cancelConfig = async () => {
@@ -72,22 +114,42 @@ class Widget extends Component {
     this.initialize(this.props.dashboardApi);
   };
 
-  changeCatId = cat => {
-    var catId = cat.key;
-    this.setState({catId});
+  updateRandomPet = () => {
+    var {petId, random} = this.state;
+    if (random === 'random-cat') {
+      var newId = petId;
+      while (newId === petId) {
+        newId = PET_IDS[Math.floor(Math.random() * 16) + 2].key;
+      }
+      petId = newId
+    }
+    this.setState({petId});
+  }
+
+  changePetId = pet => {
+    var petId = pet.key;
+    if (petId === 'random-cat') {
+      var random = petId;
+      this.setState({random})
+      this.updateRandomPet()
+    } else {
+      var random = 'none'
+      this.setState({petId, random});
+    }
   }
 
   renderConfiguration() {
-    const {isConfiguring, catId, title} = this.state;
-    const cat = CAT_IDS[0];
+    const {isConfiguring, petId, random, title} = this.state;
+    var pet = PET_IDS.find((id) => id.key === random) ||
+        PET_IDS.find((id) => id.key === petId);
 
     return (
       <div>
         <Select
-          data={CAT_IDS}
-          selected={cat}
-          onChange={this.changeCatId}
-          label="Choose cat!"
+          data={PET_IDS}
+          selected={pet}
+          onChange={this.changePetId}
+          label="Choose pet"
         />
         <Panel>
           <Button primary onClick={this.saveConfig}>{'Save'}</Button>
@@ -98,21 +160,17 @@ class Widget extends Component {
   }
 
   render() {
-    const {isConfiguring, catId, title} = this.state;
+    const {isConfiguring, petId, title} = this.state;
 
     if (isConfiguring) {
       return this.renderConfiguration();
     }
 
-    if (catId === 'random') {
-      catId = LIST_OF_CAT_IDS[Math.floor(Math.random() * LIST_OF_CAT_IDS.length)];
-    }
-
-    const url = 'images\\' + catId + '.gif';
+    const url = 'images\\' + petId + '.gif';
 
     return (
-      <div className={styles['cat-image']}>
-        <img className={styles['cat-image']} src={url}></img>
+      <div className={styles['pet-image']}>
+        <img className={styles['pet-image']} src={url}></img>
       </div>
     );
   }
